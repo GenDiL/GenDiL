1f47dc20a7e1f540a0a14c3860f280a5a001a25e
Yohann Dudouit <yohann.dudouit@gmail.com>
2025-08-26 18:03:26 -0700

    Remove dead code in ConjugateGradient.

--- include/gendil/Solvers/conjugategradient.hpp
+++ include/gendil/Solvers/conjugategradient.hpp
@@ -84,158 +84,6 @@ auto ConjugateGradient(
    return Result{ false, iters, tol_error };
 }
 
-// template<
-//    typename KernelContext,
-//    typename Operator,
-//    typename Vector >
-// GENDIL_HOST_DEVICE
-// auto ConjugateGradient(
-//    const KernelContext & kernel_conf,
-//    const Operator & A,
-//    const Vector & rhs,
-//    const Integer & max_iters,
-//    const Real & tol,
-//    Vector & x )
-// {
-//    using Result = std::tuple< bool, Integer, Real >; // Success, iteration, tolerance error
-//    using TmpVector = Vector; // TODO: Make some trait
-//    // TODO: Allocate all vectors here? Provide allocator? Some kind of memory space?
-   
-//    Integer iters;
-//    TmpVector tmp, z;
-
-//    Real tol_error;
-
-//    TmpVector residual = rhs;
-//    A( x, tmp );
-//    residual = residual - tmp; // initial residual
-
-//    Real rhs_norm = Norml2( kernel_conf, rhs );
-//    if( rhs_norm == 0 )
-//    {
-//       x = 0.0;
-//       iters = 0;
-//       tol_error = 0.0;
-//       return Result{ true, iters, tol_error };
-//    }
-//    Real residual_norm = Norml2( kernel_conf, residual );
-//    if( residual_norm < tol * rhs_norm )
-//    {
-//       iters = 0;
-//       tol_error = Sqrt( residual_norm / rhs_norm );
-//       return Result{ true, iters, tol_error };
-//    }
-
-//    TmpVector p = residual; // initial search direction
-
-//    Real abs_new = Dot( kernel_conf, residual, p );  // the square of the absolute value of r scaled by invM
-//    Integer i = 0;
-//    while( i < max_iters )
-//    {
-//       A( p, tmp );
-
-//       Real alpha = abs_new / Dot( kernel_conf, p, tmp );       // the amount we travel on dir
-//       x = x + alpha * p;                             // update solution
-//       residual = residual - alpha * tmp;                    // update residual
-
-//       residual_norm = Norml2( kernel_conf, residual );
-//       if( residual_norm < tol * rhs_norm )
-//       {
-//          tol_error = Sqrt( residual_norm / rhs_norm );
-//          iters = i;
-//          return Result{ true, iters, tol_error };
-//       }
-
-//       z = residual; // approximately solve for "A z = residual"
-
-//       Real abs_old = abs_new;
-//       abs_new = Dot( kernel_conf, residual, z );     // update the absolute value of r
-//       Real beta = abs_new / abs_old;    // calculate the Gram-Schmidt value used to create the new search direction
-//       p = z + beta * p;                 // update search direction
-//       i++;
-//    }
-//    tol_error = Sqrt( residual_norm / rhs_norm );
-//    iters = i;
-//    return Result{ false, iters, tol_error };
-// }
-
-// Preconditionned conjugate gradient algorithm
-// template<
-//    typename Operator,
-//    typename Rhs,
-//    typename Preconditioner,
-//    typename Vector >
-// GENDIL_HOST_DEVICE
-// auto ConjugateGradient(
-//    const Operator & A,
-//    const Rhs & rhs,
-//    const Preconditioner & P,
-//    const Integer & max_iters,
-//    const Real & tol,
-//    Vector & x )
-// {
-//    using Result = std::tuple< bool, Integer, Real >; // Success, iteration, tolerance error
-//    using TmpVector = Vector; // TODO: Make some trait
-//    // TODO: Allocate all vectors here? Provide allocator? Some kind of memory space?
-   
-//    Integer iters;
-//    TmpVector tmp, z;
-
-//    Real tol_error;
-
-//    TmpVector residual = rhs;
-//    A( x, tmp );
-//    residual -= tmp; // initial residual
-
-//    Real rhs_norm = Norml2( rhs );
-//    if( rhs_norm == 0 )
-//    {
-//       x = 0.0;
-//       iters = 0;
-//       tol_error = 0.0;
-//       return Result{ true, iters, tol_error };
-//    }
-//    Real residual_norm = Norml2( residual );
-//    if ( residual_norm < tol * rhs_norm )
-//    {
-//       iters = 0;
-//       tol_error = Sqrt( residual_norm / rhs_norm );
-//       return Result{ true, iters, tol_error };
-//    }
-
-//    TmpVector p;
-//    P( residual, p );      // initial search direction
-
-//    Real abs_new = Dot( residual, p );  // the square of the absolute value of r scaled by invM
-//    Integer i = 0;
-//    while( i < max_iters )
-//    {
-//       A( p, tmp );
-
-//       Real alpha = abs_new / Dot( p, tmp );        // the amount we travel on dir
-//       x += alpha * p;                             // update solution
-//       residual -= alpha * tmp;                    // update residual
-
-//       residual_norm = Norml2( residual );
-//       if( residual_norm < tol * rhs_norm )
-//       {
-//          tol_error = Sqrt( residual_norm / rhs_norm );
-//          return Result{ true, iters, tol_error };
-//       }
-
-//       P( residual, z );                // approximately solve for "A z = residual"
-
-//       Real abs_old = abs_new;
-//       abs_new = Dot( residual, z );     // update the absolute value of r
-//       Real beta = abs_new / abs_old;     // calculate the Gram-Schmidt value used to create the new search direction
-//       p = z + beta * p;                // update search direction
-//       i++;
-//    }
-//    tol_error = Sqrt( residual_norm / rhs_norm );
-//    iters = i;
-//    return Result{ false, iters, tol_error };
-// }
-
 /**
  * @brief Solve A x = rhs using the Conjugate Gradient method.
  *
@@ -269,11 +117,8 @@ template<
 >
 GENDIL_HOST_DEVICE
 auto ConjugateGradient(
-   // std::function< void(const Vector &, Vector &) > &  A,
-   // std::function< void(const Vector &, Vector &) > &  P,
    const Operator & A,
    const RHSType & rhs,
-   // std::function< Real(const Vector &, const Vector &) > & dot,
    const DotType & dot,
    const Integer max_iters,
    const Real tol,
@@ -368,13 +213,6 @@ auto ConjugateGradient(
    const Real & tol,
    Vector & x )
 {
-   // std::function< void(const Vector &, Vector &) > A_op = [&]( const Vector & x, Vector & y )
-   auto A_op = [&]( const Vector & x, Vector & y )
-   {
-      A( x, y );
-   };
-
-   // std::function< Real(const Vector &, const Vector &) > dot = [&]( const Vector & u, const Vector & v )
    auto dot = [&]( const Vector & u, const Vector & v )
    {
       return Dot( kernel_conf, u, v );
@@ -382,7 +220,7 @@ auto ConjugateGradient(
 
    Vector tmp, z, residual, p;
 
-   return ConjugateGradient( A_op, rhs, dot, max_iters, tol, x, tmp, z, residual, p );
+   return ConjugateGradient( A, rhs, dot, max_iters, tol, x, tmp, z, residual, p );
 }
 
 }
