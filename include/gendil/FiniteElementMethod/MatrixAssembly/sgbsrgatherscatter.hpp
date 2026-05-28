c004773e87b22a794451d325a1c4877e7bf0b508
Yohann Dudouit <dudouit1@llnl.gov>
2026-05-28 14:02:49 -0700

    More cleaning.

--- include/gendil/FiniteElementMethod/MatrixAssembly/sgbsrgatherscatter.hpp
+++
@@ -1,8 +0,0 @@
-// Copyright GenDiL Project Developers. See COPYRIGHT file for details.
-//
-// SPDX-License-Identifier: (BSD-3-Clause)
-
-#pragma once
-
-// Compatibility header for SGBSR gather/scatter policies.
-#include "gendil/FiniteElementMethod/MatrixAssembly/SGBSR/sgbsrgatherscatter.hpp"
